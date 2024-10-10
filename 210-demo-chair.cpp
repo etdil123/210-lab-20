0c435d92a06386843895e2e3e2a8d6cf6094cbb6
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-10-10 01:10:26 -0700

    final commit

--- 210-demo-chair.cpp
+++ 210-demo-chair.cpp
@@ -7,8 +7,6 @@ using namespace std;
 const int SIZE = 3;
 const int MIN_PRICE = 10000;
 const int MAX_PRICE = 99999;
-const int MIN_LEGS = 300;
-const int MAX_LEGS = 400;
 
 class Chair {
 private:
@@ -18,10 +16,11 @@ public:
     // constructors
     Chair() {
         prices = new double[SIZE];
-        int randL = (rand() % (MAX_LEGS - MIN_LEGS + 1) + MIN_LEGS) / (int) 100;
+        int randL = (rand() % 2) + 3;
         legs = randL;
         for (int i = 0; i < SIZE; i++){
             // randomizing double between 100.00 and 999.99
+            // code given in Lab 20 instructions
             double price = (rand() % (MAX_PRICE - MIN_PRICE + 1) + MIN_PRICE) / (double) 100;
             // storing random double into Chair prices array
             prices[i] = price;
@@ -68,32 +67,26 @@ int main() {
     Chair *chairPtr = new Chair;
     chairPtr->setLegs(4);
     chairPtr->setPrices(121.21, 232.32, 414.14);
-    cout << "\nChair created by Default Constructor - Changed with Setters" << endl;
+    cout << "\nChair Created Using Default Constructor - Changed with Setters:" << endl;
     chairPtr->print();
-    cout << endl;
-
+    
     //creating dynamic chair object with constructor
     // Uses updated parameter constructor - feeds in from parameter int AND array of prices  
     double chairPricesExample[SIZE] = {312.42, 299.99, 499.99};
     Chair *livingChair = new Chair(3, chairPricesExample);
-    cout << "Chair created by Updated Parameter Constructor" << endl;
+    cout << "\nChair Created Using Updated Parameter Constructor:" << endl;
     livingChair->print();
     delete livingChair;
     livingChair = nullptr;
 
     //creating dynamic array of chair objects
+    // Uses updated default constructor
     Chair *collection = new Chair[SIZE];
+    cout << "\nChair Created Using Updated Default Constructor:" << endl;
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
+    delete[] collection;
+    collection = nullptr;
 
-    // collection[0].setLegs(4);
-    // collection[0].setPrices(441.41, 552.52, 663.63);
-    // collection[1].setLegs(4);
-    // collection[1].setPrices(484.84, 959.59, 868.68);
-    // collection[2].setLegs(4);
-    // collection[2].setPrices(626.26, 515.15, 757.57);
-    // for (int i = 0; i < SIZE; i++)
-    //     collection[i].print();
-    
     return 0;
 }
